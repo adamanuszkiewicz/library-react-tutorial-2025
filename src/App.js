41c3ac53ea848badcc359adfa982326c5a662bac
adamanuszkiewicz <162536934+adamanuszkiewicz@users.noreply.github.com>
2025-03-30 19:49:58 -0700

    commit - library-react-tutorial- 2025

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from './components/Footer';
+import React, { useState, useEffect } from 'react';
+import Nav from './components/Nav';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Books from './pages/Books';
+import { books } from './data';
+import BookInfo from './pages/BookInfo';
+import Cart from './pages/Cart';
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, book])
+  }
+
+  useEffect(() => {
+    console.log(cart)
+  }, [cart])
+
+  /* ADDING MULTIPLE QUANTITIES TO CART / ADDING MORE THAN 1 OF THE SAME BOOK TO THE CART */
+  // function addToCart(book) {
+  //   const dupeItem = cart.find(item => +item.id === +book.id)
+
+  //   if (dupeItem) {
+  //     setCart(cart.map(item => {
+  //       if (item.id === dupeItem.id) {
+  //         return {
+  //           ...item,
+  //           quantity: item.quantity + 1,
+  //         }
+  //       }
+  //       else {
+  //         return item
+  //       }
+  //     }))
+  //   }
+  //   else {
+  //     setCart([...cart, {...book, quantity: 1}])
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   console.log(cart)
+  // }, [cart])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route path="/books/:id" render={() => <BookInfo books={books} addToCart={addToCart} />} />
+        <Route path="/cart" render={() => <Cart books={books} />} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/BookInfo.jsx
@@ -0,0 +1,97 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import Book from "../components/ui/Book";
+import Price from "../components/ui/Price";
+import Rating from "../components/ui/Rating";
+
+const BookInfo = ({ books, addToCart, cart }) => {
+  const { id } = useParams();
+  const book = books.find((book) => +book.id === +id);
+  const [added, setAdded] = useState(false)
+
+  function addBookToCart(book) {
+    setAdded(true);
+    addToCart(book);
+  }
+
+  function bookInCart() {
+    return cart.find(book => book.id === +id)
+  }
+
+  return (
+    <div id="books__body">
+      <main id="books__main">
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <Link to="/books" className="book__link">
+                <FontAwesomeIcon icon="arrow-left" />
+              </Link>
+              <Link to="/book" className="book__link">
+                <h2 className="book__selected--title--top">Books</h2>
+              </Link>
+            </div>
+            <div className="book__selected">
+              <figure className="book__selected--figure">
+                <img src={book.url} alt="" className="book__selected--img" />
+              </figure>
+              <div className="book__selected--description">
+                <h2 className="book__selected--title">{book.title}</h2>
+                <Rating rating={book.rating} />
+                <div className="book__selected--price">
+                  <Price
+                    originalPrice={book.originalPrice}
+                    salePrice={book.salePrice}
+                  />
+                </div>
+                <div className="book__summary">
+                  <h3 className="book__summary--title">Summary</h3>
+                  <p className="book__summary--para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Fuga libero illum quae voluptatem corporis doloribus
+                    laboriosam? Eos sunt dolore nam dignissimos libero, a
+                    dolorum beatae ducimus laborum delectus quos assumenda.
+                  </p>
+                  <p className="book__summary--para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Fuga libero illum quae voluptatem corporis doloribus
+                    laboriosam? Eos sunt dolore nam dignissimos libero, a
+                    dolorum beatae ducimus laborum delectus quos assumenda.
+                  </p>
+                </div>
+                {bookInCart() ? (
+                  <Link to={`/cart/${book.id}`} className="book__link">
+                  <button className="btn">Checkout</button>
+                   </Link>
+                ) : (
+                  <button className="btn" onClick={() => addBookToCart(book)}>
+                    Add to cart
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <h2 className="book__selected--title--top">Recomended Books</h2>
+            </div>
+            <div className="books">
+              {books
+                .filter((book) => book.rating === 5 && +book.id !== +id)
+                .slice(0, 4)
+                .map((book) => (
+                  <Book book={book} key={book.id} />
+                ))}
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default BookInfo;
